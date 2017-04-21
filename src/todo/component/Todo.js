63e3a7c7a014da0bbb6e6b9c2bf4c25b23b1ad45
ubuntu <shota.oda+github@bizreach.co.jp>
2017-04-21 17:45:06 +0900

    実装したで

--- src/todo/component/Todo.js
+++ src/todo/component/Todo.js
@@ -1,5 +1,34 @@
-import React, {Component, PropTypes} from 'react';
+import React, { Component, PropTypes } from 'react';
 import TodoList from './TodoList';
+
+const s = {
+  layout: {
+    backgroundColor: '#fff',
+    boxShadow: '0 2px 4px 0 rgba(0, 0, 0, 0.2), 0 25px 50px 0 rgba(0, 0, 0, 0.1);',
+    margin: '0 0 40px 0',
+  },
+  title: {
+    fontSize: '100px',
+    fontWeight: '100',
+    textAlign: 'center',
+    color: 'rgba( 175, 47, 47, 0.15)',
+    textRendering:  'optimizeLegibility',
+  },
+  input: {
+    padding: '16px 16px 16px 60px',
+    background: 'rgba(0, 0, 0, 0.003)',
+    boxShadow: 'inset 0 -2px 1px rgba(0,0,0,0.03)',
+    margin: '0',
+    width: '100%',
+    fontSize: '24px',
+    fontFamily: 'inherit',
+    fontWeight: 'inherit',
+    lineHeight: '1.4em',
+    border: '0',
+    outline: 'none',
+    boxSizing: 'border-box',
+  }
+};
 
 class Todo extends Component {
 
@@ -17,7 +46,6 @@ class Todo extends Component {
   };
 
   renderTodo(todos) {
-    console.log(todos);
     if (todos.length > 0) {
       return todos.map(todo => (
           <li key={todo.key}>{todo.text}</li>
@@ -29,16 +57,21 @@ class Todo extends Component {
   render() {
     return (
       <div>
-        <h1>TODO</h1>
-        <form className="commentForm" onSubmit={this.handleSubmit}>
-          <input type="text" placeholder="name" ref="name"/>
-          <input type="submit" value="Todo に追加する"/>
-        </form>
-        <TodoList
-          todo={this.props.todo}
-          handleToggleTodo={this.props.handleToggleTodo}
-          handleDeleteTodo={this.props.handleDeleteTodo}
-        />
+        <h1 style={s.title}>todos</h1>
+        <section style={s.layout} className="paper">
+          <section>
+            <form className="commentForm" onSubmit={this.handleSubmit}>
+              <input style={s.input} type="text" placeholder="な〰にやりますか〰" ref="name" />
+            </form>
+            <TodoList
+              todo={this.props.todo}
+              handleToggleTodo={this.props.handleToggleTodo}
+              handleDeleteTodo={this.props.handleDeleteTodo}
+            />
+          </section>
+          <footer className="paper-foot" style={{ height: '0' }} />
+        </section>
+        <footer style={{ margin: '65px auto 0' }} />
       </div>
     );
   }
--- src/todo/component/TodoList.js
+++ src/todo/component/TodoList.js
@@ -1,5 +1,47 @@
 import React, {Component} from 'react';
 
+const s = {
+  item: {
+    fontSize: '24px',
+    whiteSpace: 'pre-line',
+    wordBreak: 'break-all',
+    padding: '15px 60px 15px 15px',
+    marginLeft: '45px',
+    marginRight: '30px',
+    display: 'block',
+    lineHeight: '1.2',
+    transition: 'color 0.4s',
+  },
+  toggle: {
+    WebkitAppearance: 'none',
+    appearance: 'none',
+    outline: 'none',
+    position: 'absolute',
+    margin: 'auto 0',
+    height: '40px',
+    width: '40px',
+    top: 0,
+    bottom: 0,
+  },
+  delete: {
+    cursor: 'pointer',
+    color: '#ddd',
+    borderRadius: '50%',
+    border: '1px solid #ddd',
+    position: 'absolute',
+    verticalAlign: 'middle',
+    lineHeight: '30px',
+    fontSize: '20px',
+    textAlign: 'center',
+    right: '30px',
+    top: '0px',
+    height: '30px',
+    width: '30px',
+    bottom: '0px',
+    margin: "auto 0",
+  }
+};
+
 class TodoList extends Component {
 
   handleToggleTodo(key) {
@@ -10,13 +52,35 @@ class TodoList extends Component {
     this.props.handleDeleteTodo(key);
   };
 
+  constructor(prop) {
+    super(prop)
+  }
+
   renderTodo(todos) {
+
     if (todos.length > 0) {
-      return todos.map(todo => (
-          <div key={todo.key}>
-            <li onClick={e => this.handleToggleTodo(todo.key)}>{todo.text} {todo.completed.toString()}</li>
-            <p onClick={e => this.handleDeleteTodo(todo.key)}>削除</p>
-          </div>
+      return todos.map((todo, index) => (
+          <li style={{ borderBottom: '1px solid #ededed' }} key={todo.key}>
+            <div style={{ position: 'relative' }}>
+              <input
+                type="checkbox"
+                checked={todo.completed}
+                style={s.toggle}
+                onChange={() => this.handleToggleTodo(todo.key)}
+              />
+              <label
+                style={s.item}
+              >
+                {todo.text}
+              </label>
+              <span
+                style={s.delete}
+                onClick={e => this.handleDeleteTodo(todo.key)}
+              >
+                ☓
+              </span>
+            </div>
+          </li>
         )
       );
     }
